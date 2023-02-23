0fd39dd572ddca9b2a991fa178cb5b5c525fcca2
mikehoyle <michael.a.hoyle@gmail.com>
2023-02-23 12:20:15 -0800

    pre-alpha title screen

---
+++ Assets/Scripts/MainTitle/TitleSceneManager.cs
@@ -0,0 +1,11 @@
+﻿using Common;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MainTitle {
+  public class TitleSceneManager : MonoBehaviour {
+    public void OnPlayButtonClick() {
+      SceneManager.LoadScene(Scenes.Name.Overworld.SceneName());
+    }
+  }
+}
